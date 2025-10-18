2b017e9f13fc7f2d7f5ae7b4bbc15a017f84082b
Rushabh <161584263+Rushabh-00@users.noreply.github.com>
2025-10-18 14:13:00 +0530

    Update build_aarch32.sh

--- build_aarch32.sh
+++ build_aarch32.sh
@@ -7,6 +7,11 @@ set -e
 bash get_source.sh $TARGET_VERSION
 cd openjdk
 
+# --- THE CRITICAL STEP: APPLY OUR LOCAL PATCHES ---
+echo "Applying patches for Java $TARGET_VERSION..."
+# Use git apply to apply all .diff files from the correct patch directory
+git apply ../jre_${TARGET_VERSION}/*.diff
+
 # Configure the build
 bash ./configure \
     --openjdk-target=arm-linux-androideabi \
@@ -15,13 +20,13 @@ bash ./configure \
     --with-toolchain-type=gcc \
     --with-extra-cflags="-fPIC -Wno-error -mfloat-abi=softfp -mfpu=vfp" \
     --with-extra-cxxflags="-fPIC -Wno-error -mfloat-abi=softfp -mfpu=vfp" \
-    --with-extra-ldflags="-Wl,-rpath-link=$JAVA_HOME/jre/lib/arm"
+    --with-extra-ldflags="-Wl,-rpath-link=$JAVA_HOME/jre/lib/arm" \
+    --with-ndk=$NDK_PATH
 
 # Run the build
 make images
 
 # After building, find the created JRE image and package it
 cd build/linux-arm-release/images
-# The folder is named 'jre' for Java 8 and 'jre-server' for 17/21
 JRE_FOLDER_NAME=$(find . -type d -name "jre*" | head -n 1)
 tar -cJf ../../../../jre${TARGET_VERSION}-aarch32.tar.xz $JRE_FOLDER_NAME
